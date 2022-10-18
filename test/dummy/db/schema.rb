2dfc9831293031b33dcc07a68cda0acee2905ee7
Tiago Casanova <tiago.casanova.pt@gmail.com>
2022-10-18 16:52:36 +0100

    Remove old table reference in gem dummy schema

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -14,18 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_14_183623) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "auth2_users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_auth2_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_auth2_users_on_reset_password_token", unique: true
-  end
-
   create_table "ombu_labs_auth_users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
